5a663235d8caeaf1eccf1166794d268fa58848f7
golamrabbani93 <golamrabbani9373@gmail.com>
2023-10-29 11:48:13 +0600

    get product by ids

--- routes/handleProducts.js
+++ routes/handleProducts.js
@@ -26,4 +26,25 @@ router.get('/', async (req, res) => {
 	}
 });
 
+router.get('/:id', async (req, res) => {
+	try {
+		const id = req.params.id;
+		const productQuery = {_id: id};
+		const product = await productsCollection.findOne(productQuery);
+		if (product._id) {
+			res.status(200).json({
+				message: 'success',
+				data: product,
+			});
+		} else {
+			res.status(404).json({
+				message: 'Not Found',
+				data: 0,
+			});
+		}
+	} catch (error) {
+		res.status(500).json({message: 'there is an error in server'});
+	}
+});
+
 module.exports = router;
