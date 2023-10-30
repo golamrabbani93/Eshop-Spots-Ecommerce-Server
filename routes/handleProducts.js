0fbe266aadef5835a40fd0527f31f61c14d8fd24
golamrabbani93 <golamrabbani9373@gmail.com>
2023-10-30 23:06:11 +0600

    get new arrivals and best seller product

--- routes/handleProducts.js
+++ routes/handleProducts.js
@@ -9,6 +9,37 @@ const ProductSchema = require('../schemas/ProductsSchema');
 const productsCollection = new mongoose.model('Product', ProductSchema);
 router.get('/', async (req, res) => {
 	try {
+		const query = req.query;
+		// !get only new arrival products
+		if (query.status) {
+			const newArrivalProducts = await productsCollection.find(query);
+			if (newArrivalProducts.length > 0) {
+				res.status(200).json({
+					message: 'success',
+					data: newArrivalProducts,
+				});
+			} else {
+				res.status(404).json({
+					message: 'Not Found',
+					data: 0,
+				});
+			}
+		}
+		// !get best seller products
+		if (query.bestSeller) {
+			const newArrivalProducts = await productsCollection.find({best_seller: true});
+			if (newArrivalProducts.length > 0) {
+				res.status(200).json({
+					message: 'success',
+					data: newArrivalProducts,
+				});
+			} else {
+				res.status(404).json({
+					message: 'Not Found',
+					data: 0,
+				});
+			}
+		}
 		const newArrivalProducts = await productsCollection.find();
 		if (newArrivalProducts.length > 0) {
 			res.status(200).json({
