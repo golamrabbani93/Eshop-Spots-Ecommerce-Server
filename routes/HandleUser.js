122acc530986c5b2a6f65be08e1fc6c5ef22c309
Golam Rabbani <golamrabbani9373@gmail.com>
2024-01-18 14:40:13 +0600

    get user from database with user email

--- routes/HandleUser.js
+++ routes/HandleUser.js
@@ -8,7 +8,28 @@ const UserSchema = require('../schemas/UserSchema');
 // !create User Collection
 const userCollection = mongoose.model('User', UserSchema);
 
-router.get('/', async (req, res) => {});
+router.get('/', async (req, res) => {
+	try {
+		//* get email from client side
+		const userEmail = req.query.email;
+		//* get user from database
+		const userDetails = await userCollection.findOne({email: userEmail});
+
+		if (userDetails._id) {
+			return res.status(200).json({
+				message: 'success',
+				user: userDetails,
+			});
+		} else {
+			return res.status(404).json({
+				message: 'No Data Found',
+				userDetails: {},
+			});
+		}
+	} catch (error) {
+		return res.status(500).json({message: 'there is an error in server'});
+	}
+});
 router.post('/', async (req, res) => {
 	try {
 		const userdata = req.body;
